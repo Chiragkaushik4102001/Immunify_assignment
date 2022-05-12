c81f1ad2d35023c61e6a0fbb8351440283eaa628
Chirag Kaushik <60777553+Chiragkaushik4102001@users.noreply.github.com>
2022-05-12 11:11:04 +0530

    Add files via upload

---
+++ indexes6.js
@@ -0,0 +1,120 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXGJ3mqOX7o8P1GyD75EwfPEg6xErGY5s",
+  authDomain: "book-store-f7d94.firebaseapp.com",
+  projectId: "book-store-f7d94",
+  storageBucket: "book-store-f7d94.appspot.com",
+  messagingSenderId: "803201965465",
+  appId: "1:803201965465:web:5ffd42c58ebceed1830708",
+  measurementId: "G-XHQVLKY41B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log('This is ES6 version of Project 2');
+class Book {
+    constructor(name, author, type) {
+        this.name = name;
+        this.author = author;
+        this.type = type;
+    }
+}
+
+class Display {
+    add(book) {
+        console.log("Adding to UI");
+        let tableBody = document.getElementById('tableBody');
+        let uiString = `<tr>
+                            <td>${book.name}</td>
+                            <td>${book.author}</td>
+                            <td>${book.type}</td>
+                        </tr>`;
+        tableBody.innerHTML += uiString;
+    }
+
+    clear() {
+        let libraryForm = document.getElementById('libraryForm');
+        libraryForm.reset();
+    }
+
+    validate(book) {
+        if (book.name.length < 2 || book.author.length < 2) {
+            return false
+        }
+        else {
+            return true;
+        }
+    }
+
+    show(type, displayMessage) {
+        let message = document.getElementById('message');
+        let boldText;
+        if(type==='success'){
+            boldText = 'Success';
+        }
+        else{
+            boldText = 'Error!';
+        }
+        message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                                <strong>${boldText}:</strong> ${displayMessage}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                                </button>
+                            </div>`;
+        setTimeout(function () {
+            message.innerHTML = ''
+        }, 5000);
+    
+    }
+}
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('YOu have submitted library form');
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let Coding = document.getElementById('Coding');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (Coding.checked) {
+        type = Coding.value;
+    }
+
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    let display = new Display();
+
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been added successfully')
+    }
+    else {
+        // Show error to the user
+        display.show('danger', 'Sorry you cannot add this book');
+    }
+
+    e.preventDefault();
+}
